9298058d1e36baa110dccfb481b7ff156b6f23e8
Sangkun-svg <gims4752@gmail.com>
2022-06-14 03:08:46 +0900

    feat : write sample code about type-aliases

---
+++ class-note/4_type-aliases.ts
@@ -0,0 +1,20 @@
+type Person = {
+  name: string;
+  age: number;
+};
+interface Person2 {
+  name: string;
+  age: number;
+}
+
+const sangkunSvg: Person2 = {
+  // interface
+  name: "sag",
+  age: 23,
+};
+
+const han: Person = {
+  // type-aliases
+  name: "han",
+  age: 23,
+};
